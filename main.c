ef2f248bb44b9e8ef331836062c6537d0bfe3ba3
magda0705 <magdutza8@gmail.com>
2024-04-01 22:11:12 +0300

    adaugat checker

---
+++ main.c
@@ -0,0 +1,9 @@
+   #include <stdio.h>
+   int main (int argc, char* argv[]) {
+       printf("Number of input files: %d\n", argc);
+       printf("Exec name: %s\n", argv[0]);
+       printf("File1: %s\n", argv[1]);
+       printf("File2: %s\n", argv[2]);
+       printf("File3: %s\n", argv[3]);
+       // FILE *f_in = fopen( argv[1], "r" );
+return 0; }
